8814ac3a06ee3d79392910e2d3167826d2b7a096
smlimonparvez <smlimonparvez@gmail.com>
2025-05-31 08:31:10 +0600

    galaxy apartment start

---
+++ src/auth/AuthProvider.jsx
@@ -0,0 +1,104 @@
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import axios from "axios";
+import app from "../firebase/firebase.init";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // new user create
+  const signUp = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // new user sign in
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // google sign in
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, provider);
+  };
+
+  // const sign out
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  // user profile update
+  const userProfileUpdate = async (updateData) => {
+    await updateProfile(auth.currentUser, updateData);
+    setUser((prevUser) => ({ ...prevUser, ...updateData }));
+  };
+
+  // reset password
+  const resetPassword = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  // auth state
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      // console.log("currentUser", currentUser);
+      if (currentUser?.email) {
+        const user = { email: currentUser.email };
+        axios
+          .post("http://localhost:5173/jwt", user, { withCredentials: true })
+          .then((res) => {
+            console.log(res.data);
+            setLoading(false);
+          });
+      } else {
+        axios
+          .post("http://localhost:5173/logout", {}, { withCredentials: true })
+          .then((res) => {
+            console.log("logout", res.data);
+            setLoading(false);
+          });
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const userInfo = {
+    user,
+    setUser,
+    loading,
+    setLoading,
+    signUp,
+    signIn,
+    googleSignIn,
+    logOut,
+    userProfileUpdate,
+    resetPassword,
+  };
+
+  return (
+    <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
